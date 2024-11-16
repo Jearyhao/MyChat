e7fdd0d7153cbb5a2f9f556060a1cc77ffa94802
Jearyhao <18732979706@163.com>
2024-11-16 20:33:09 +0800

    complete login UI

--- Server/Server.pro
+++ Server/Server.pro
@@ -1,4 +1,4 @@
-QT       += core gui network
+QT       += core gui network sql
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
@@ -31,3 +31,6 @@ FORMS += \
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
+
+RESOURCES += \
+    assets.qrc
--- Server/main.cpp
+++ Server/main.cpp
@@ -3,11 +3,28 @@
 #include "revisedialog.h"
 #include "enrolldialog.h"
 #include <QApplication>
-
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QtSql>
+#include <QSqlError>
+#include <QDebug>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     LoginServer w;
     w.show();
+    QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+        db.setHostName("127.0.0.1");//你的IP地址
+        db.setDatabaseName("openGauss");
+        db.setUserName("jrh"); //用户名
+        db.setPassword("741QAZabc");//密码
+        db.setPort(26000); //opengauss端口号为26000
+        bool flag = db.open();
+        if (flag){
+            qDebug() << "success";
+        } else{
+            qDebug() << "failed";
+        }
+
     return a.exec();
 }
