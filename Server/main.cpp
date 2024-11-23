84b51829d8ca5acbd9b814932cb1a0d70721cf5f
Jearyhao <112200710@fzu.edu.cn>
2024-11-23 14:32:11 +0800

    clear server UI

--- Server/main.cpp
+++ Server/main.cpp
@@ -10,7 +10,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    LoginServer w;
+    ServerDialog w;
     w.show();
     QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
     db.setHostName("127.0.0.1");//你的IP地址
